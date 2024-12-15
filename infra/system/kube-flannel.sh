c3a1f88e2438d5c98deeeff21508cb4285959b5c
cdxker <fdenzell@gmail.com>
2024-12-15 01:28:05 +0000

    adding base install

--- infra/system/kube-flannel.sh
+++
@@ -1,5 +0,0 @@
-kubectl create ns kube-flannel
-kubectl label --overwrite ns kube-flannel pod-security.kubernetes.io/enforce=privileged
-
-helm repo add flannel https://flannel-io.github.io/flannel/
-helm install flannel --set podCidr="192.168.0.0/16" --namespace kube-flannel flannel/flannel
